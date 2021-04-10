5d4e13b7704c664e901626480e376a6e385d45a9
08rose08 <naudinrose@gmail.com>
2021-04-11 00:12:29 +0200

    Fix script

--- script.js
+++ script.js
@@ -3,16 +3,29 @@ const COMPLIMENTS = [
     "Tu es plus cool qu'un chaton déguisé en licorne !",
     "Tu es plus magique que la poudre de perlimpinpin !",
     "Il y a l'ordinaire et il y a toi !",
-    "Fais toi confiance, tu es capable d'avoir des idée géniales !",
+    "Fais toi confiance, tu es capable d'avoir des idées géniales !",
     "Tu es fantastique comme un jour férié un lundi !",
     "Tu es encore plus extraordinaire qu'une place assise dans le métro à 9h du matin !",
-    "Tu envoies plus de rêves que le marchand de sable",
-    "Tu ne crois peut-être plus aux licornes, mais elles n'ont jamais arrêté de croire en toi ;)"
+    "Tu envoies plus de rêves que le marchand de sable !",
+    "Tu ne crois peut-être plus aux licornes, mais elles n'ont jamais cessé de croire en toi !",
+    "Tu es la personne la plus douce, compréhensive, battante, déterminée, résiliente et maline que je connaisse !"
 ]
+
+function removeCompliment() {
+    let container = document.getElementById("compliment-text");
+    container.classList.remove('added');
+    container.classList.add('removed');
+    generateCompliment();
+}
 
 function generateCompliment() {
     let nb = Math.floor(Math.random() * COMPLIMENTS.length);
-    //console.log(COMPLIMENTS[nb]);
-    let container = document.getElementById("compliment");
-    container.innerText = COMPLIMENTS[nb];
+    setTimeout(() => {addCompliment(COMPLIMENTS[nb]);}, 1000);
+}
+
+function addCompliment(compliment) {
+    let container = document.getElementById("compliment-text");
+    container.classList.remove('removed');
+    container.classList.add('added');
+    container.innerText = compliment;
 }
